95136baf8dca4ccb8adf44c4e90758735f0b494d
Maarten de Groot <m.l.d.groot@student.tue.nl>
2023-06-05 12:06:54 +0200

    removed arrays

--- MPPT_c/MPPT.c
+++ MPPT_c/MPPT.c
@@ -17,48 +17,36 @@ int set_fast = 0;
 int set_bound = 0;
 int set_slow = 0;
 int direction = 0;
-float slow_filter_array[slow_filter_size];
-float medium_filter_array[medium_filter_size];
-float fast_filter_array[fast_filter_size];
+float filter_array[slow_filter_size];
 
 int MPPT(int voltage, int amps, float duty_cycle_previous){
 
     //array shifting
     for(int i = slow_filter_size-1; i >= 0; i--){
-        slow_filter_array[i] = slow_filter_array[i-1];
-    }
-
-    for(int i = medium_filter_size-1; i >= 0; i--){
-        medium_filter_array[i] = medium_filter_array[i-1];
-    }
-
-    for(int i = fast_filter_size-1; i >= 0; i--){
-        fast_filter_array[i] = fast_filter_array[i-1];
+        filter_array[i] = filter_array[i-1];
     }
 
     //adding power at front of arrays
     float power = voltage * amps;
-    slow_filter_array[1] = power;
-    medium_filter_array[1] = power;
-    fast_filter_array[1] = power;
+    filter_array[1] = power;
 
     //calculating mean values
     float sum = 0;
     for(int i = 0; i < slow_filter_size; i++){
-        sum = sum + slow_filter_array[i];
+        sum = sum + filter_array[i];
     }
     float slow_filter_average = sum / slow_filter_size;
     float power_lower_bound = lower_bound_value * slow_filter_average;
 
     sum = 0;
     for(int i = 0; i < medium_filter_size; i++){
-        sum = sum + medium_filter_array[i];
+        sum = sum + filter_array[i];
     }
     float medium_filter_average = sum / medium_filter_size;
 
     sum = 0;
     for(int i = 0; i < fast_filter_size; i++){
-        sum = sum + fast_filter_array[i];
+        sum = sum + filter_array[i];
     }
     float fast_filter_average = sum / fast_filter_size;
 
